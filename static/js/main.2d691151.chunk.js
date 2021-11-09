6ea71042d7f43b4cb39450bb9ec90ecb16f649d3
Seth Strouf <seth.strouf@doane.edu>
2021-11-08 20:37:59 -0600

    Updates

--- static/js/main.2d691151.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),o=c.n(n),r=c(3),s=c.n(r),i=(c(8),c(9),c(0));var a=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("header",{className:"App-header",children:Object(i.jsx)("h1",{children:"Seth Strouf's Portfolio"})})})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
-//# sourceMappingURL=main.2d691151.chunk.js.map
