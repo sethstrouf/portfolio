ce6a525863fd32ba3594388506711f040f8e7267
Seth Strouf <seth.strouf@doane.edu>
2021-11-12 17:27:11 -0600

    Change homepage to correct domain

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -15,28 +15,30 @@ const ContactForm = () => {
   }
 
   return (
-    <form id="contact-form" onSubmit={handleSubmit}>
-      <div>
-        <label>Name</label>
+    <form id="contact-form" onSubmit={handleSubmit} className="p-4">
+
+      <div className="form-div">
+        <label htmlFor="user_name">Name</label>
+      </div><div>
+        <input type="text" name="user_name" placeholder="Name" required 
+               className="form-element" />
       </div>
-      <div>
-        <input type="text" name="user_name" required className="form-element" />
+
+      <div className="form-div">
+        <label htmlFor="user_email">Email</label>
+      </div><div>
+        <input type="email" name="user_email" required className="form-element" />
+      </div>
+
+      <div className="form-div">
+        <label htmlFor="message">Message</label>
+      </div><div>
+        <textarea name="message" required className="form-element"></textarea>
       </div>
 
       <div>
-        <label>Email</label>
+        <input type="submit" value="Send" className="my-4 px-12 font-bold rounded bg-pink-100 border border-pink-700 hover:bg-pink-200" />
       </div>
-      <div>
-        <input type="email" name="user_email" required required className="form-element" />
-      </div>
-
-      <div>
-        <label>Message</label>
-      </div>
-      <div>
-        <textarea name="message" required required className="form-element"></textarea>
-      </div>
-        <input type="submit" value="Send" />
     </form>
   )
 }
