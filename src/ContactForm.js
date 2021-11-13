2772da21361cdb3c401fd417d87095d3e661e1f2
Seth Strouf <seth.strouf@doane.edu>
2021-11-13 16:22:53 -0600

    Update styling for contact form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -16,26 +16,25 @@ const ContactForm = () => {
 
   return (
     <form id="contact-form" onSubmit={handleSubmit} 
-          className="mb-6 py-4 mx-auto bg-white shadow-lg rounded-lg 
-                     w-10/12 border border-gray-300
-                     md:w-8/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12">
+          className="mb-6 py-4 mx-auto bg-white shadow-lg rounded-lg text-gray-500
+                     w-10/12 border md:w-8/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12">
 
       {/* form-div and input-element @applied in index.css */}
       <div className="form-div">
         <label htmlFor="user_name">Name</label>
-        <input type="text" name="user_name" placeholder="Name" required 
+        <input type="text" name="user_name" required 
                className="input-element" />
       </div>
 
       <div className="form-div">
         <label htmlFor="user_email">Email</label>
-        <input type="email" name="user_email" placeholder="Email" required 
+        <input type="email" name="user_email" required 
                className="input-element" />
       </div>
 
       <div className="form-div">
         <label htmlFor="message">Message</label>
-        <textarea name="message" placeholder="Message" required 
+        <textarea name="message" required 
                   className="input-element h-48">
         </textarea>
       </div>
