47cb651c57bfcba5d430f0e5f5579a029eca3073
Seth Strouf <seth.strouf@doane.edu>
2021-11-13 06:38:21 -0600

    Center header

--- src/App.js
+++ src/App.js
@@ -9,23 +9,27 @@ export default function App() {
 
       <header>
         <NavBar itemStyle="text-gray-50 hover:text-green-200" />
-        <div className="md:flex md:justify-center md:items-center">
-          <h1 className="name pt-4 text-4xl
-                         md:p-8 md:my-8 md:no-underline md:text-5xl md:border-r-4 md:border-green-400
-                         xl:text-6xl">
-            <strong>Hi, I'm Seth!</strong>
-          </h1>
-          <p className="pt-1 text-xl
-                        md:p-8 md:text-2xl
-                        xl:text-3xl">
-            <em>a full-stack web developer</em>
-          </p>
+        
+        <div className="my-4 md:flex md:justify-center md:items-center 
+                        md:my-12 2xl:my-12">
+          <div className="pt-4 md:border-r-4 md:border-green-400 md:w-6/12 
+                          md:text-right md:py-12 2xl:py-16">
+            <h1 className="name text-4xl
+                           md:no-underline md:text-5xl md:pr-10
+                           xl:text-6xl">
+              <strong>Hi, I'm Seth!</strong>
+            </h1>
+          </div>
+          <div className="pt-1 md:w-6/12 md:text-left">
+            <p className="text-xl md:pl-8 md:text-2xl xl:text-3xl">
+              <em>a full-stack web developer</em>
+            </p>
+          </div>
         </div>
-          <p className="pt-8
-                        md:text-lg
-                        xl:text-xl">
+          <p className="md:text-lg xl:text-xl">
             <a href="https://drive.google.com/file/d/1ge77AXAoAvWQNTWVLrRyusPm516Vs70E/view?usp=sharing"
-                className="py-1 px-6 rounded bg-pink-100 border-4 border-pink-700 border-double hover:bg-pink-200">
+                className="py-0.5 px-4 md:px-6 xl:px-8 rounded bg-pink-100 border-4 
+                         border-pink-700 border-double hover:bg-pink-200">
               View my resume!
             </a>
           </p>
