bb45e1f8f43260e6195dec6bb65e4ba1f7201ab8
Seth Strouf <seth.strouf@doane.edu>
2021-11-18 04:45:38 -0600

    Add accent to resume

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
             <a href="https://drive.google.com/file/d/1ge77AXAoAvWQNTWVLrRyusPm516Vs70E/view?usp=sharing"
                 className="py-1 px-4 rounded bg-green-400 hover:bg-green-300  
                           font-bold md:py-2 md:px-6 md:text-xl xl:px-8 2xl:text-3xl">
-              Resume
+              Resumé
             </a>
           </p>
       </header>
